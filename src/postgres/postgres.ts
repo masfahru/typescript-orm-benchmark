2db6d2b8c2bb84898e29b414ad71ecb4534ecb0e
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-10-27 05:32:17 +0700

    enable transform column names into camelCase in Postgres.js

--- src/postgres/postgres.ts
+++ src/postgres/postgres.ts
@@ -7,7 +7,7 @@ if (!options.max) {
   delete options.max;
 }
 
-const sql = postgres(options);
+const sql = postgres(Object.assign({ transform: postgres.camel }, options));
 
 export const postgresGetUser = async (id: number) =>
   await sql`SELECT * FROM users WHERE id = ${id}`.then((rows) => rows[0]);
