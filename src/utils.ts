bff8435153a5455235c2bb557169b5403501c55c
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2025-02-24 06:55:22 +0700

    Update utils.ts
    `faker.internet.userName()` is deprecated, changed to `faker.internet.username()`

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker';
 import Bun from 'bun';
 
 export const createRandomUser = async () => ({
-  username: faker.internet.userName(),
+  username: faker.internet.username(),
   email: faker.internet.email(),
   password: await Bun.password.hash(faker.internet.password()),
   name: faker.person.fullName(),
